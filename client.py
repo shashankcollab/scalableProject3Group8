d796debd64b4d1203c2e6acce2a4f25bc108e3a3
Rupasmita Devi <devir@scss.tcd.ie>
2020-12-12 19:14:51 +0000

    code for client to close a connection

--- client.py
+++ client.py
@@ -7,6 +7,9 @@ HOST = sys.argv[1]  # The server's hostname or IP address
 PORT = 33000 # The port used by the server
 msg = sys.argv[2]
 while True:
+    data = input("Please specify 'yes/no' if you want to close the connection: ")
+    if data[0:3].lower() == "yes":
+        sys.exit(0)
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((HOST, PORT))
