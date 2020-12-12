e7390027ee532822e7f3a36d4835fcb8410a2de0
shashank <sha2nk1991@gmail.com>
2020-12-12 18:23:41 +0000

    Initial Commit
    Creating new branch

---
+++ client.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+ 
+import socket
+import sys
+ 
+HOST = sys.argv[1]  # The server's hostname or IP address
+PORT = 33000 # The port used by the server
+while True:
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            s.sendall(b'Hello, world')
+            data = s.recv(1024)
+        
+        print('Received', repr(data))
+    except Exception as e:
+        print('No server alive dude')
+        sys.exit(0)
---
+++ server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+ 
+import socket
+import sys
+HOST = socket.gethostname()  # Standard loopback interface address (localhost)
+PORT = 33000        # Port to listen on (non-privileged ports are > 1023)
+while(True):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.bind((HOST, PORT))
+            s.listen(5)
+            while True:
+                data = input("Please enter a message: ")
+                if data[0:1].lower() == "q":
+                    sys.exit(0)
+                else:
+                    print("{}{}".format(HOST,PORT))
+                    conn, addr = s.accept()
+                    with conn:
+                        print('Connected by', addr)
+                        while True:
+                            data = conn.recv(1024)
+                            if not data:
+                                break
+                            conn.sendall(data)
+        except KeyboardInterrupt as e:
+            print('server is disconnecting')
+            sys.exit(0)
