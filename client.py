3569e3e458fc12fad44349aba0087ef0934839ed
shashank <sha2nk1991@gmail.com>
2020-12-13 19:05:28 +0000

    testing changes

--- client.py
+++ client.py
@@ -2,29 +2,35 @@
  
 import socket
 import sys
+import json
  
-_HOST = sys.argv[1]  if len(sys.argv) > 1 else None  # The server's hostname or IP address
+HOST = sys.argv[1]  # The server's hostname or IP address
 PORT = 33000 # The port used by the server
-#msg = sys.argv[2]
+msg = sys.argv[2]
 while True:
-    HOST = input("Please type the new peer or hit Enter to continue : \n") or _HOST
-    while HOST is None:
-        HOST = input("NO PEER SPECIFIED! Please type peer: \n") or _HOST
-    print("Connected to the peer: ", HOST)
-    msg = input("Please enter the msg you want to send: \n")
+    
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((HOST, PORT))
             s.sendall(msg.encode())
-            data = s.recv(1024)
-        
-        print('Received: ', repr(data.decode()))
+            data = s.recv(1024).decode()
+            d = json.loads(data)
+            print('Suggesiont from POD {} is {} because of weather condition temperature is {} humidity {}: '.format(HOST, d["Alert"],d["temperature"],d["humidity"]))
+
+
+            with open('temp_log_client.json', 'w') as f:
+                print ('Storing file..')
+                if not data:
+                    break
+                f.write('\n')
+                f.write(data)
+                s.close()
+            print ('Succesfully recieved')
     except Exception as e:
-        print('No server alive dude')
+        print('No server alive dude{}'.format(e))
         sys.exit(0)
 
     data = input("Please specify 'yes/no' if you want to close the connection: ")
     if data[0:3].lower() == "yes" or data[0:1].lower() == "y":
         sys.exit(0)
-    _HOST = HOST
 
--- server.py
+++ server.py
@@ -1,53 +1,43 @@
-
 #!/usr/bin/env python3
-
+ 
 import socket
-import threading 
 import sys
 HOST = socket.gethostname()  # Standard loopback interface address (localhost)
 PORT = 33000        # Port to listen on (non-privileged ports are > 1023)
-connections = []
-peers = []
-
-def broadcast_peers(peers):
-    for connection in connections:
-        peers = 'Peers currently in the network: {}'.format(peers)
-        connection.send(peers.encode())
 while(True):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         try:
             s.bind((HOST, PORT))
             s.listen(5)
             while True:
-                msgStr = input("Please enter a message or type 'yes' to close the connection:  ")
-                if msgStr[0:1].lower() == "y" or msgStr[0:3].lower() == "yes":
+                msgStr = input("Please enter the message to send to receivers \nOr type 'close' to close the connection:  ")
+                if msgStr[0:1].lower() == "c" or msgStr[0:3].lower() == "close":
                     sys.exit(0)
                 else:
                     print("{}{}".format(HOST,PORT))
                     conn, addr = s.accept()
-                    connections.append(conn)
-                    peers.append(addr)
-                    print('Connected peers are: ', peers)
-                    c_thread = threading.Thread(target=broadcast_peers, args = (peers))
-                    c_thread.daemon = True
-                    c_thread.start()
-
-                    with conn:
-                        print('Connected by', addr)
-                        while True:
-                            data = conn.recv(1024)
-                            data = data.decode()
-
-                            print('Message sent by peer: ', data)
-                            if not data:
-                                break
-                            elif data == 'connect':
-                                #msgStr = 'Hello From the Server {}'.format(HOST)
-                                conn.send(msgStr.encode())
-                            else:
-                                conn.send(b'No available data')
-
+                    try:
+                        with conn:
+                                print('Connected by', addr)
+                                while True:
+                                    data = conn.recv(1024)
+                                    data = data.decode()
+                                    print('Message sent by client: ', data)
+                                    if not data:
+                                        break
+                                    elif data == 'connect':
+                                        filename = 'temp_log.json'
+                                        f = open(filename, 'rb')
+                                        msgStr= f.read(1024)
+                                        # msgStr = 'Hello From the Server {}{}'.format(HOST,data)
+                                        print('Hello From the Server {} is with data {}'.format(msgStr,data))
+                                        conn.sendall(msgStr)
+                                    else:
+                                        conn.sendall(b'No available data')
+                    except Exception as e:
+                        print(e)
+                        sys.exit(0)
+                
         except KeyboardInterrupt as e:
             print('server is disconnecting')
             sys.exit(0)
-
